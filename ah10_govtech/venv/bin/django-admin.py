d1fd682386df1b2099f4e20ba56de694d046fe3e
Laxmi Thebe <laxmi.thebe@gmail.com>
2017-05-28 08:35:06 +0300

    removed venv

--- ah10_govtech/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nguyenvantai/Desktop/ah10-hel-govtech/ah10_govtech/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
